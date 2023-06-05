2028c03e981e96ff6ea542082c75e6c781e3ef8c
MinMinChit <44768348+MinMinChit@users.noreply.github.com>
2023-06-05 15:17:02 +0630

    Add files via upload

--- lib/gradient_linear_progress.dart
+++ lib/gradient_linear_progress.dart
@@ -2,62 +2,48 @@ library custom_ui;
 
 import 'package:flutter/material.dart';
 
-class GradientLinearProgressIndicator extends StatelessWidget {
+class CustomLinearProgressIndicator extends StatelessWidget {
   final double progress;
-  final double height;
+  final double? height;
   final double? width;
-  final TextStyle? titleStyle;
-  final TextStyle? progressStyle;
-  final String? title;
-  final List<Color>? colors;
+  final BoxDecoration? backDecoration;
+  final BoxDecoration? decoration;
+  final Duration? animatedDuration;
 
-  const GradientLinearProgressIndicator({
+  const CustomLinearProgressIndicator({
     Key? key,
     required this.progress,
-    this.height = 20,
+    this.height = 8,
     this.width,
-    this.title,
-    this.titleStyle,
-    this.progressStyle,
-    this.colors,
+    this.backDecoration,
+    this.decoration,
+    this.animatedDuration,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              title ?? '',
-              style: titleStyle,
-            ),
-            Text(
-              '${(progress * 100).round()}%',
-              style: progressStyle,
-            ),
-          ],
-        ),
-        const SizedBox(height: 6),
         Stack(
           children: [
             Container(
               height: height,
               width: width ?? MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Color(0xFFFFFFFF),
-                    Color(0xFFEBEBEB),
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(100),
-              ),
+              decoration: backDecoration ??
+                  BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [
+                        Colors.grey,
+                        Colors.grey,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
             ),
             TweenAnimationBuilder(
+              curve: Curves.linear,
               tween: Tween<double>(begin: 0, end: progress),
-              duration: const Duration(seconds: 4),
+              duration: animatedDuration ?? const Duration(seconds: 0),
               builder: (BuildContext context, double value, Widget? child) {
                 return LayoutBuilder(
                   builder:
@@ -65,12 +51,16 @@ class GradientLinearProgressIndicator extends StatelessWidget {
                     return Container(
                       width: value * boxConstraints.maxWidth,
                       height: height,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        gradient: LinearGradient(
-                          colors: colors ?? [Colors.black, Colors.white],
-                        ),
-                      ),
+                      decoration: decoration ??
+                          BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Colors.red,
+                                Colors.black,
+                              ],
+                            ),
+                          ),
                     );
                   },
                 );
