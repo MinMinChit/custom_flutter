a59803f6af3bfe62b896139108836844dea8e453
MinMinChit <44768348+MinMinChit@users.noreply.github.com>
2023-06-10 09:45:12 +0630

    Add files via upload

--- lib/gradient_linear_progress.dart
+++ lib/gradient_linear_progress.dart
@@ -3,71 +3,89 @@ library custom_ui;
 import 'package:flutter/material.dart';
 
 class CustomLinearProgressIndicator extends StatelessWidget {
-  final double progress;
+  final double? progress;
   final double? height;
   final double? width;
-  final BoxDecoration? backDecoration;
-  final BoxDecoration? decoration;
+  final BorderRadius? borderRadius;
+  final Color? color;
+  final Color? baseColor;
+  final Gradient? gradient;
+  final Gradient? baseGradient;
   final Duration? animatedDuration;
 
   const CustomLinearProgressIndicator({
     Key? key,
-    required this.progress,
+    this.progress,
     this.height = 8,
     this.width,
-    this.backDecoration,
-    this.decoration,
+    this.borderRadius,
+    this.gradient,
+    this.baseGradient,
     this.animatedDuration,
+    this.color,
+    this.baseColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Stack(
-          children: [
-            Container(
-              height: height,
-              width: width ?? MediaQuery.of(context).size.width,
-              decoration: backDecoration ??
-                  BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Colors.grey,
-                        Colors.grey,
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(100),
+        progress == null
+            ? Container(
+                width: width ?? MediaQuery.of(context).size.width,
+                height: height,
+                decoration: BoxDecoration(
+                  color: color ?? Colors.grey,
+                  gradient: gradient,
+                  borderRadius: borderRadius,
+                ),
+                child: ClipRRect(
+                  borderRadius: borderRadius,
+                  child: LinearProgressIndicator(
+                    backgroundColor: Colors.transparent,
+                    valueColor:
+                        AlwaysStoppedAnimation<Color>(baseColor ?? Colors.blue),
                   ),
-            ),
-            TweenAnimationBuilder(
-              curve: Curves.linear,
-              tween: Tween<double>(begin: 0, end: progress),
-              duration: animatedDuration ?? const Duration(seconds: 0),
-              builder: (BuildContext context, double value, Widget? child) {
-                return LayoutBuilder(
-                  builder:
-                      (BuildContext context, BoxConstraints boxConstraints) {
-                    return Container(
-                      width: value * boxConstraints.maxWidth,
-                      height: height,
-                      decoration: decoration ??
-                          BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            gradient: const LinearGradient(
-                              colors: [
-                                Colors.red,
-                                Colors.black,
-                              ],
+                ),
+              )
+            : Stack(
+                children: [
+                  //base
+                  Container(
+                    height: height,
+                    width: width ?? MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: baseColor ?? Colors.grey,
+                      borderRadius: borderRadius,
+                      gradient: baseGradient,
+                    ),
+                  ),
+
+                  //progress
+                  TweenAnimationBuilder(
+                    curve: Curves.linear,
+                    tween: Tween<double>(begin: 0, end: progress),
+                    duration: animatedDuration ?? const Duration(seconds: 0),
+                    builder:
+                        (BuildContext context, double value, Widget? child) {
+                      return LayoutBuilder(
+                        builder: (BuildContext context,
+                            BoxConstraints boxConstraints) {
+                          return Container(
+                            width: value * boxConstraints.maxWidth,
+                            height: height,
+                            decoration: BoxDecoration(
+                              borderRadius: borderRadius,
+                              color: color ?? Colors.blue,
+                              gradient: gradient,
                             ),
-                          ),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ],
+              ),
       ],
     );
   }
